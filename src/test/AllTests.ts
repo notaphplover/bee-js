4a555850170e7ad64874a0310d006df20141e070
Roberto Pintos López <roberto@secture.com>
2019-07-23 19:21:31 +0200

    Removed inference mechanisms.

--- src/test/AllTests.ts
+++ src/test/AllTests.ts
@@ -1,12 +1,8 @@
-import { CtorInferenceAlgorithmTest } from './inference/CtorInferenceAlgorithmTest';
-import { InferenceChainTest } from './inference/InferenceChainTest';
 import { ITest } from './ITest';
 import { SerializerTest } from './serialization/SerializerTest';
 
 export class AllTest implements ITest {
   public performTests(): void {
-    new CtorInferenceAlgorithmTest().performTests();
-    new InferenceChainTest().performTests();
     new SerializerTest().performTests();
   }
 }
